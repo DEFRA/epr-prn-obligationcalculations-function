5101571d6643963d24d8ace6e7a0d80bed64d37e
Saywa Singh Boparai <saywa.boparai@eviden.com>
2024-09-09 14:34:10 +0100

    Initial setup of repository

---
+++ src/EPR.PRN.ObligationCalculation.Application/Configs/CommonDataApiConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace EPR.PRN.ObligationCalculation.Application.Configs
+{
+    public class CommonDataApiConfig
+    {
+        public const string SectionName = "CommonDataApiConfig";
+        public string BaseUrl { get; set; } = null!;
+        public string Endpoint { get; set; } = null!;
+        public int Timeout { get; set; }
+        public int ServiceRetryCount { get; set; }
+        public int ApiVersion { get; set; }
+    }
+}
