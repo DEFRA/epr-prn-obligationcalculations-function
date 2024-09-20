ffcc5bfbbec3dd5b50564f459c1ae13cdddf44cb
jayakrishnanappusamy <jayakrishnan.appusamy@atos.net>
2024-09-20 18:17:20 +0100

    Program class excluded from coverage

--- src/EPR.PRN.ObligationCalculation.Function/Program.cs
+++ src/EPR.PRN.ObligationCalculation.Function/Program.cs
@@ -4,22 +4,33 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics.CodeAnalysis;
 
-var host = new HostBuilder()
-    .ConfigureFunctionsWebApplication()
-    .ConfigureServices((hostingContext, services) =>
+namespace EPR.PRN.ObligationCalculation.Function
+{
+    [ExcludeFromCodeCoverage]
+    public static class Program
     {
-        services.AddApplicationInsightsTelemetryWorkerService();
-        services.ConfigureFunctionsApplicationInsights();
-        services.AddHttpClient();
-        services.AddScoped<ISubmissionsDataService, SubmissionsDataService>();
-        services.AddScoped<IAppInsightsProvider, AppInsightsProvider>();
-        services.AddScoped<IServiceBusProvider, ServiceBusProvider>();
-        services.ConfigureOptions(hostingContext.Configuration);
-        services.AddHttpClients();
-        services.AddAzureClients(hostingContext.Configuration);
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+        .ConfigureFunctionsWebApplication()
+        .ConfigureServices((hostingContext, services) =>
+        {
+            services.AddApplicationInsightsTelemetryWorkerService();
+            services.ConfigureFunctionsApplicationInsights();
+            services.AddHttpClient();
+            services.AddScoped<ISubmissionsDataService, SubmissionsDataService>();
+            services.AddScoped<IAppInsightsProvider, AppInsightsProvider>();
+            services.AddScoped<IServiceBusProvider, ServiceBusProvider>();
+            services.ConfigureOptions(hostingContext.Configuration);
+            services.AddHttpClients();
+            services.AddAzureClients(hostingContext.Configuration);
 
-    })
-    .Build();
+        })
+        .Build();
 
-await host.RunAsync();
+            await host.RunAsync();
+        }
+    }
+}
