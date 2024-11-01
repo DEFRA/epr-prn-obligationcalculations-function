f3ba3415b9945657f1e265c49fc79f11c5d27e3e
jayakrishnanappusamy <jayakrishnan.appusamy@atos.net>
2024-11-01 08:40:31 +0000

    Removed submissionId from model

--- src/EPR.PRN.ObligationCalculation.Application/DTOs/ApprovedSubmissionEntity.cs
+++ src/EPR.PRN.ObligationCalculation.Application/DTOs/ApprovedSubmissionEntity.cs
@@ -5,9 +5,8 @@ namespace EPR.PRN.ObligationCalculation.Application.DTOs;
 [ExcludeFromCodeCoverage]
 public class ApprovedSubmissionEntity
 {
-    public Guid SubmissionId { get; set; }
+    public Guid OrganisationId { get; set; }
     public string? SubmissionPeriod { get; set; }
     public string? PackagingMaterial { get; set; }
     public double PackagingMaterialWeight { get; set; }
-    public Guid OrganisationId { get; set; }
 }
