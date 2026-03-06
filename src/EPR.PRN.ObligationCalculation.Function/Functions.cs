9042af9373af420bf9ccb603fc0eefeba7ef869d
Tim Peel <tim.peel@equalexperts.com>
2026-03-06 16:21:55 +0000

    Try and exclude from code coverage checks

--- src/EPR.PRN.ObligationCalculation.Function/Functions.cs
+++ src/EPR.PRN.ObligationCalculation.Function/Functions.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace EPR.PRN.ObligationCalculation.Function;
 
+[ExcludeFromCodeCoverage]
 public static class Functions
 {
     public const string StoreApprovedSubmissionsFunction = nameof(StoreApprovedSubmissionsFunction);
